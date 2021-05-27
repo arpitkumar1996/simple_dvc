55d98be64643167df4d5d8d00ae243db040794a7
arpit199600 <arpitkumar482@gmail.com>
2021-05-27 10:40:02 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(
+
+    name = "src",
+    version="0.0.1",
+    description = "It is a Wine Q package",
+    author = "Arpit Kumar",
+    packages = find_packages()
+
+)
